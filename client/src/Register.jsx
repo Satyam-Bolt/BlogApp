df8c832d2d4a1f01b8b76643f7f83285f0dad765
Satyam-Bolt <34099550+Satyam-Bolt@users.noreply.github.com>
2023-08-01 12:11:54 +0530

    Update Register.jsx

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/register', {username, email, password})
+        axios.post('https://blog-app-app.vercel.app/register', {username, email, password})
         .then(res => navigate('/login'))
         .catch(err => console.log(err))
     }
