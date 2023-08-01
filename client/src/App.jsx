4744e43233e1e35c174a6b8b38115b2a54c1d280
Satyam-Bolt <34099550+Satyam-Bolt@users.noreply.github.com>
2023-08-01 12:06:30 +0530

    Update App.jsx
    backend conn vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   axios.defaults.withCredentials = true;
   useEffect(() => {
-    axios.get('http://localhost:3001/')
+    axios.get('https://blog-app-app.vercel.app/')
     .then(user => {
       setUser(user.data)
     })
